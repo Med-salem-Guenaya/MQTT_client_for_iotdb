f739fb2b67e144338ff3e4c5417cc0a5bc50852a
Med-salem-Guenaya <medsalemahmed99@gmail.com>
2024-05-06 02:08:25 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.fusesource.mqtt.client.BlockingConnection;
+import org.fusesource.mqtt.client.MQTT;
+import org.fusesource.mqtt.client.QoS;
+
+import java.net.URISyntaxException;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        MQTT mqtt = new MQTT();
+        mqtt.setHost("192.168.56.101", 1883);
+        mqtt.setUserName("root");
+        mqtt.setPassword("root");
+
+        BlockingConnection connection = mqtt.blockingConnection();
+        connection.connect();
+
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            String payload = String.format("{\n" +
+                    "\"device\":\"root.sg.d1\",\n" +
+                    "\"timestamp\":%d,\n" +
+                    "\"measurements\":[\"s1\"],\n" +
+                    "\"values\":[%f]\n" +
+                    "}", System.currentTimeMillis(), random.nextDouble());
+
+            connection.publish("root.sg.d1.s1", payload.getBytes(), QoS.AT_LEAST_ONCE, false);
+        }
+
+        connection.disconnect();
+
+
+
+    }
+}
